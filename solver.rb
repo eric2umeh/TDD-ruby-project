66d8c5b12cc5a16a16c31188326411d8a9c720e7
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-04-20 16:05:18 +0300

    Update for fizzbuzz in solver.rb

--- solver.rb
+++ solver.rb
@@ -12,7 +12,9 @@ class Solver
   end
 
   def fizzbuzz(num)
-    if (num % 3).zero?
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
       'fizz'
     elsif (num % 5).zero?
       'buzz'
