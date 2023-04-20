1df27683b7b4298e779683c391d1158900fea23d
Eric <eric2umeh@yahoo.com>
2023-04-20 13:42:00 +0100

    Run test for 0!

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,5 +5,9 @@ describe Solver do
     it 'returns the factorial of a positive integer' do
       expect(subject.factorial(3)).to eq(6)
     end
+
+    it 'returns 1 for 0' do
+      expect(subject.factorial(0)).to eq(1)
+    end
   end
 end
